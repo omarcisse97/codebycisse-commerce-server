e6f6c097a86928cd113416853af1f3a7f3ae5faa
omar cisse <omarcisse97@knights.ucf.edu>
2025-06-05 02:11:53 -0400

    Reverted post commit

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,8 +1,10 @@
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
+
 if (process.env.NODE_ENV === 'development') {
   loadEnv(process.env.NODE_ENV, process.cwd())
 }
+
 
 console.log("DATABASE_URL:", process.env.DATABASE_URL)
 console.log("REDIS_URL:", process.env.REDIS_URL || process.env.redisUrl)
@@ -17,8 +19,5 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
-  },
-  admin: {
-    backendUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000",
   }
 })
